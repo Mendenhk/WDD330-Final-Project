b6432c192c5ea70816fffd571ade1f9a9c7d1233
kris mendenhall <kris_mendenhall@yahoo.com>
2026-06-08 20:17:03 +0700

    corrected files by copiying and pasting correct content.  Un commented load footer header, and querySelector element for creeating list.

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -75,8 +75,8 @@ export async function loadTemplate(path) {
 }
 
 export async function loadHeaderFooter() {
-  const headerTemplate = await loadTemplate("./partials/header.html");
-  const footerTemplate = await loadTemplate("./partials/footer.html");
+  const headerTemplate = await loadTemplate("/src/partials/header.html");
+  const footerTemplate = await loadTemplate("/src/partials/footer.html");
 
   const headerElement = document.getElementById("main-header");
   const footerElement = document.getElementById("main-footer");
