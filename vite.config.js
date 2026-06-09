da4237b58933e322ebd4496e1c19ec06ab14bd7d
kris mendenhall <kris_mendenhall@yahoo.com>
2026-06-09 11:22:33 +0700

    attempts to get npm run start to work

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+export default defineConfig({
+  root: "src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        temples: resolve(__dirname, "src/temples/index.html"),
+      },
+    },
+  },
+});
