33f6b805db1327f7a09d34b26a1d3a8c210ae86b
kris mendenhall <kris_mendenhall@yahoo.com>
2026-06-09 12:14:23 +0700

    added temple images and started template funmction

--- src/js/temples.js
+++ src/js/temples.js
@@ -1,24 +1,42 @@
 //kriston: next: transfer "getData" method from the class ExternalServices from externalservices.mjs to this file and use to extract temples.json
-
 import { loadHeaderFooter} from "./utils.mjs";
+
+loadHeaderFooter();
 
 async function getTemples() {
   const response = await fetch('/src/json/temples.json');
-  const responseText = await response.text();
-  console.log("hello world");
-  let data = responseText;
-  try {
-    data = responseText ? JSON.parse(responseText) : {};
-  } catch (error) {
-    data = responseText || response.statusText;
-  }
   if (response.ok) {
-    return data;
+    return await response.json();
   } else {
     throw {
       name: "servicesError",
-      message: data || response.statusText,
+      message: response.statusText,
       status: response.status,
     };
   }
 }
+
+async function init() {
+  const temples = await getTemples();
+  console.log(temples); // remove once confirmed working
+  templeCardTemplate(temples);
+}
+
+init();
+
+function templeCardTemplate(temple) {
+  return `
+    <li class="temple-card">
+      <img src="${temple.image}" alt="${temple.name}">
+      <h3>${temple.name}</h3>
+      <p class="khmer-name">${temple.khmerName}</p>
+      <p>${temple.uniqueFact}</p>
+      <ul>
+        ${temple.highlights.map(h => `<li>${h}</li>`).join("")}
+      </ul>
+      <p class="hours">Hours: ${temple.openingHours}</p>
+      <p class="tip"><strong>Tip:</strong> ${temple.tip}</p>
+    </li>
+  `;
+}
+
